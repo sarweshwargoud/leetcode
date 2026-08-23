8c69d31f681b53f30ed6da4af002ce9067160a23
Sarweshwar Buddolla <b.sarweshwar445@gmail.com>
2026-08-23 20:02:35 +0530

    Time: 5 ms (72.69%), Space: 19.1 MB (87.19%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+
+        st=str(x)
+
+        if st==st[::-1]:
+            return True
+        else :
+            return False
+
+
+        
